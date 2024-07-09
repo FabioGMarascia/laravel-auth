288f30487cc417e28d020bf0de13c665596a13c4
FabioGMarascia <fabiogaspare.marascia@gmail.com>
2024-07-09 16:56:35 +0200

    posts migration

--- database/migrations/2024_07_09_135857_create_posts_table.php
+++ database/migrations/2024_07_09_135857_create_posts_table.php
@@ -14,6 +14,12 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('project_title');
+            $table->text('description');
+            $table->string('collaborators');
+            $table->string('framework');
+            $table->date('start_project');
+            $table->date('end_project');
         });
     }
 
--- routes/web.php
+++ routes/web.php
@@ -20,8 +20,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('posts', PostController::class);
-
 Route::middleware(['auth'])
     ->prefix('admin') //definisce il prefisso "admin/" per le rotte di questo gruppo
     ->name('admin.') //definisce il pattern con cui generare i nomi delle rotte cioè "admin.qualcosa"
@@ -31,6 +29,8 @@ Route::middleware(['auth'])
         // - il percorso "/" diventa "admin/"
         // - il nome della rotta ->name("dashboard") diventa ->name("admin.dashboard")
         Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+
+        Route::resource('posts', PostController::class);
     });
 
 require __DIR__ . '/auth.php';

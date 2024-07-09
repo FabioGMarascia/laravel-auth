514b702516da5abb1eee27b463b1a58a67838a56
sidane17 <khalilsidane89@gmail.com>
2024-07-09 17:05:19 +0200

    added faker seeder

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -4,13 +4,24 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use Faker\Generator as Faker;
+use App\Models\Post;
 class PostSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
+        for ($i = 0; $i < 10; $i++) {
+            $post= new Post();
+            $post->project_title=$faker->name;
+            $post->description=$faker->text;
+            $post->collaborators=$faker->name;
+            $post->framework=$faker->name;
+            $post->start_project=$faker->date();
+            $post->end_project=$faker->date();
+            $post->save();
+            }  
     }
 }

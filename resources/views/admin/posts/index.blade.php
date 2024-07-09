ee2426ad2a8b1060558ee5822e6db952118ebcad
FabioGMarascia <fabiogaspare.marascia@gmail.com>
2024-07-09 20:36:33 +0200

    index style

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -4,22 +4,26 @@
 
 @section('content')
     <div class="container">
-        <div class="row pt-5">
+        <div class="row pt-5 justify-content-center">
             @foreach ($posts as $post)
-                <div class="card p-4 m-3  " style="width: 18rem; background-color:rgb(255, 154, 154)">
+                <div class="col-3 card p-4 m-3 border border-2 border-black"
+                    style=" background-color:rgba(255, 153, 0, 0.723)">
                     <img src="{{ $post['thumb'] }}" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h5 class="card-title">{{ $post['project_title'] }}</h5>
                         <p class="card-text">{{ $post['description'] }}</p>
-                        <div class="row d-flex">
-                            <a href="{{ route('admin.posts.edit', $post->id) }}" class="btn btn-primary">Edit</a>
-                            <a href="{{ route('admin.posts.show', $post->id) }}" class="btn btn-primary">Dettaglio</a>
+                        <div class="row d-flex justify-content-center">
+                            <a href="{{ route('admin.posts.edit', $post->id) }}" class="btn btn-primary mb-2 w-75">Edit</a>
+                            <a href="{{ route('admin.posts.show', $post->id) }}"
+                                class="btn btn-primary mb-2 w-75 px-0">Dettaglio</a>
 
-                            <form action="{{ route('admin.posts.destroy', $post->id) }}" method="POST">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="btn btn-danger w-100 ">Delete</a>
-                            </form>
+                            <div class="px-0 w-75">
+                                <form action="{{ route('admin.posts.destroy', $post->id) }}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger w-100">Delete</a>
+                                </form>
+                            </div>
 
 
                         </div>

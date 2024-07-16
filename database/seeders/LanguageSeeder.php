2fc7dec932aa0878a8ef61b0a4f1dde7b031c032
Lorenzo D'Addazio <ldaddazio11@gmail.com>
2024-07-16 15:51:34 +0200

    Added new elements in languague seeder

--- database/seeders/LanguageSeeder.php
+++ database/seeders/LanguageSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Language;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,24 @@ class LanguageSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $techJS = new Language();
+        $techJS->name = "JavaScript";
+        $techJS->icon = "fa-brands fa-js";
+        $techJS->save();
+
+        $techVue = new Language();
+        $techVue->name = "VueJS";
+        $techVue->icon = "fa-brands fa-vuejs";
+        $techVue->save();
+
+        $techPHP = new Language();
+        $techPHP->name = "PHP";
+        $techPHP->icon = "fa-brands fa-php";
+        $techPHP->save();
+
+        $techLaravel = new Language();
+        $techLaravel->name = "Laravel";
+        $techLaravel->icon = "fa-brands fa-laravel";
+        $techLaravel->save();
     }
 }

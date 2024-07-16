6eb20b9668553e7b1c3dbc575200270245167fe0
sidane17 <khalilsidane89@gmail.com>
2024-07-16 15:54:38 +0200

    migrate pivot

---
+++ database/migrations/2024_07_16_134924_create_post_language_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('post_language', function (Blueprint $table) {
+            $table->unsignedBigInteger('post_id');
+            $table->foreign('post_id')->references('id')->on('post');
+
+            $table->unsignedBigInteger('languege_id');
+            $table->foreign('user_id')->references('id')->on('language');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('post_language');
+    }
+};
